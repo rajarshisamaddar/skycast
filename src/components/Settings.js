b09e24efc1d6556320495190e7dd7fa00802b2f9
rajarshisamaddar <itsrajarshi@gmail.com>
2023-07-26 22:12:16 +0530

    Modal update 🚀

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -28,12 +28,12 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
   bgcolor: "background.default",
   border: "none",
+  width: "22rem",
   borderRadius: "8px",
   boxShadow: 24,
-  p: 4,
+  p: 3.5,
 };
 
 export default function Settings() {
@@ -101,96 +101,94 @@ export default function Settings() {
 
   const { logout } = useAuth0();
   return (
-    <div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-        disableAutoFocus
-      >
-        <Box sx={style} align="center">
-          <Typography id="modal-modal-title" variant="h4" component="h2">
-            SkyCast Settings{" "}
-            <Tooltip title="Sign out from SkyCast">
-              <LogoutIcon
-                cursor="pointer"
-                onClick={() =>
-                  logout({
-                    logoutParams: { returnTo: window.location.origin },
-                  })
-                }
-                sx={{ "&:hover": { color: "error.light" } }}
-              />
-            </Tooltip>
-          </Typography>
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+      disableAutoFocus
+    >
+      <Box sx={style} align="center">
+        <Typography id="modal-modal-title" variant="h4" component="h2">
+          SkyCast Settings{" "}
+          <Tooltip title="Sign out from SkyCast">
+            <LogoutIcon
+              cursor="pointer"
+              onClick={() =>
+                logout({
+                  logoutParams: { returnTo: window.location.origin },
+                })
+              }
+              sx={{ "&:hover": { color: "error.light" } }}
+              fontSize="small"
+            />
+          </Tooltip>
+        </Typography>
+        <FormControl sx={{ mt: 4 }} fullWidth>
+          <InputLabel id="demo-multiple-name-label">
+            Choose Temperature Unit 🌡️
+          </InputLabel>
+          <Select
+            labelId="demo-multiple-name-label"
+            id="demo-multiple-name"
+            input={<OutlinedInput label="Choose Temperature Unit 🌡️" />}
+            value={unit}
+            onChange={handleUnit}
+          >
+            <MenuItem value="C">Centigrade - °C</MenuItem>
+            <MenuItem value="F">Fahrenheit - °F</MenuItem>
+          </Select>
+        </FormControl>
 
-          <FormControl sx={{ mt: 4 }} fullWidth>
-            <InputLabel id="demo-multiple-name-label">
-              Choose Temperature Unit 🌡️
-            </InputLabel>
-            <Select
-              labelId="demo-multiple-name-label"
-              id="demo-multiple-name"
-              input={<OutlinedInput label="Choose Temperature Unit 🌡️" />}
-              value={unit}
-              onChange={handleUnit}
-            >
-              <MenuItem value="C">Centigrade - °C</MenuItem>
-              <MenuItem value="F">Fahrenheit - °F</MenuItem>
-            </Select>
-          </FormControl>
+        <FormControl sx={{ mt: 3 }} fullWidth>
+          <InputLabel id="demo-multiple-name-label">
+            Select Preferred Theme 🎨
+          </InputLabel>
+          <Select
+            labelId="demo-multiple-name-label"
+            id="demo-multiple-name"
+            input={<OutlinedInput label="Select Preferred Theme 🎨" />}
+            value={theme}
+            onChange={handleTheme}
+          >
+            <MenuItem value="dark">Dark</MenuItem>
+            <MenuItem value="light">Light</MenuItem>
+          </Select>
+        </FormControl>
 
+        {fav.length > 0 && (
           <FormControl sx={{ mt: 3 }} fullWidth>
             <InputLabel id="demo-multiple-name-label">
-              Select Preferred Theme 🎨
+              Pick a Favorite Location 📍
             </InputLabel>
             <Select
               labelId="demo-multiple-name-label"
               id="demo-multiple-name"
-              input={<OutlinedInput label="Select Preferred Theme 🎨" />}
-              value={theme}
-              onChange={handleTheme}
+              input={<OutlinedInput label="Pick a Favorite Location 📍" />}
+              onChange={(e) => {
+                fav.map((obj) => {
+                  if (obj.city === e.target.value) {
+                    dispatch(currentLoc(obj.loc));
+                    const temp = {
+                      city: obj.city,
+                      region: obj.region,
+                    };
+                    dispatch(currentArea(temp));
+                    toast.success("Favourite Location Selected! 📍", {
+                      theme: theme,
+                    });
+                  }
+                });
+              }}
+              value={cArea.city}
             >
-              <MenuItem value="dark">Dark</MenuItem>
-              <MenuItem value="light">Light</MenuItem>
+              {fav.map((obj) => {
+                return <MenuItem value={obj.city}>{obj.city}</MenuItem>;
+              })}
             </Select>
           </FormControl>
-
-          {fav.length > 0 && (
-            <FormControl sx={{ mt: 3 }} fullWidth>
-              <InputLabel id="demo-multiple-name-label">
-                Pick a Favorite Location 📍
-              </InputLabel>
-              <Select
-                labelId="demo-multiple-name-label"
-                id="demo-multiple-name"
-                input={<OutlinedInput label="Pick a Favorite Location 📍" />}
-                onChange={(e) => {
-                  fav.map((obj) => {
-                    if (obj.city === e.target.value) {
-                      dispatch(currentLoc(obj.loc));
-                      const temp = {
-                        city: obj.city,
-                        region: obj.region,
-                      };
-                      dispatch(currentArea(temp));
-                      toast.success("Favourite Location Selected! 📍", {
-                        theme: theme,
-                      });
-                    }
-                  });
-                }}
-                value={cArea.city}
-              >
-                {fav.map((obj) => {
-                  return <MenuItem value={obj.city}>{obj.city}</MenuItem>;
-                })}
-              </Select>
-            </FormControl>
-          )}
-        </Box>
-      </Modal>
-    </div>
+        )}
+      </Box>
+    </Modal>
   );
 }
